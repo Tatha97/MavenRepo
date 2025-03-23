46a412a2eebda3d3efeb77f5625cc91a594bc6e0
Tathagata Sanyal <tathagata.s.1997.ts@gmail.com>
2025-03-23 19:58:56 +0530

    Commiting Maven Project

---
+++ mini/src/mytest/demo.java
@@ -0,0 +1,36 @@
+package mytest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.epam.healenium.SelfHealingDriver;
+
+public class demo {
+
+	
+	WebDriver delegate;
+	SelfHealingDriver driver;
+	@BeforeMethod
+	public void setupDriver() {
+try {
+		System.setProperty("webdriver.chrome.driver","C:\\chromedriver-win32\\chromedriver.exe");
+		delegate = new ChromeDriver();
+		driver= SelfHealingDriver.create(delegate);
+}
+	catch(Exception e) {
+		e.printStackTrace();
+	}
+	}
+	
+	@Test
+	public void test() {
+		
+		try{
+			driver.get("https://www.amazon.in/");
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
